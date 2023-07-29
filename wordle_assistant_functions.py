5f2f69147eca1d7b56d060ecb0c96c57a02706b6
KAI MAURIN-JONES <kmaurinj@student.ubc.ca>
2023-07-29 11:47:23 -0400

    big fixes

--- wordle_assistant_functions.py
+++ wordle_assistant_functions.py
@@ -343,13 +343,13 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
     `random_target`: bool
         if True, randomly chooses a target word from all words within `word_list`. If False, passed target word must be used instead
     `verbose`: bool
-        if True, # prints progress and explanation of how function solves the puzzle. If False, # prints only the guessed word at each guess.
+        if True, # st.writes progress and explanation of how function solves the puzzle. If False, # st.writes only the guessed word at each guess.
     `drama`: float or int
         if int provided, each guess' output is delayed by that number of seconds, else each output is shown as quickly as possible. For ~dRaMaTiC eFfEcT~
     `return_stats`: bool
-        if True, # prints nothing and returns a dictionary of various statistics about the function's performance trying to solve the puzzle
+        if True, # st.writes nothing and returns a dictionary of various statistics about the function's performance trying to solve the puzzle
     `record`: bool
-        if True, creates a .txt file with the same information # printed according to the indicated verbosity
+        if True, creates a .txt file with the same information # st.writeed according to the indicated verbosity
 
     Returns:
     ------
@@ -368,13 +368,13 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
         sugg_words.append(word)
 
     if guess not in word_list:
-        # print ("Guess word not in passed word list.\nOnly words within the given word list are valid.")
-        # print (f"Here are some examples of valid words from the passed word list.\n\t{sugg_words[:10]}")
+        # st.write ("Guess word not in passed word list.\nOnly words within the given word list are valid.")
+        # st.write (f"Here are some examples of valid words from the passed word list.\n\t{sugg_words[:10]}")
         return None
     
     if target not in word_list:
-        # print ("Target word not in passed word list.\nOnly words within the given word list are valid.")
-        # print (f"Here are some examples of valid words from the passed word list.\n\t{sugg_words[-10:]}")
+        # st.write ("Target word not in passed word list.\nOnly words within the given word list are valid.")
+        # st.write (f"Here are some examples of valid words from the passed word list.\n\t{sugg_words[-10:]}")
         return None
 
     if random_guess == True:
@@ -447,24 +447,23 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
         # guess_num += 1 # each time the guess is processed
         if return_stats == False:
             if guess_num == 1:
-                print("-----------------------------\n")
+                st.write("-----------------------------\n")
 
         if guess == target:
             # stats_dict['target_guessed'] = True
             if return_stats == False:
                 if guess_num == 1:
-                    # print(f"Congratulations! The Wordle has been solved in {guess_num} guess, that's amazingly lucky!")
-                    print(f"The starting word and target word are the same. Try entering two different words to see how the puzzle can be solved.")
-                    # print(f"The target word was {target}")
-                
-                
+                    # st.write(f"Congratulations! The Wordle has been solved in {guess_num} guess, that's amazingly lucky!")
+                    st.write(f"The starting word and target word are the same. Try entering two different words to see how the puzzle can be solved.")
+                    # st.write(f"The target word was {target}")
+            
                     perfect_letts_per_guess.append(5)
                     wrong_pos_per_guess.append(0)
                     wrong_letts_per_guess.append(0)
             break
             
         if return_stats == False:
-            print(f"**Guess {guess_num}: '{guess}'**")
+            st.write(f"**Guess {guess_num}: '{guess}'**")
 
         guess_set = set()
         wrong_pos_set = set()
@@ -522,13 +521,13 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
                     if (letter, pos) not in incorrect_positions:
                         incorrect_positions.append((letter, pos))
 
-        # sorting lists of tuples just to make them look nice in the # printout
+        # sorting lists of tuples just to make them look nice in the # st.writeout
         incorrect_positions = sorted(incorrect_positions, key = operator.itemgetter(1), reverse = False)
         perfect_letters = sorted(perfect_letters, key = operator.itemgetter(1), reverse = False)
 
         #### all words that have correct letters in incorrect spots -- so they can be excluded efficiently
         
-        # print(incorrect_positions)
+        # st.write(incorrect_positions)
         
         for word in word_list:
             word_set = set()
@@ -550,13 +549,13 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
 
         if return_stats == False:
             if verbose == True:
-                print(f"Letters in correct positions:\n\t{perfect_letters}\n")
-                print(f"Letters in incorrect positions:\n\t{incorrect_positions}\n")
-                # print (f"Letters to guess again:\n\t{sorted(list(next_letters), reverse = False)}\n")
-                print(f"Letters to not guess again:\n\t{sorted(list(dont_guess_again), reverse = False)}\n") # works
+                st.write(f"Letters in correct positions:\n\t{perfect_letters}\n")
+                st.write(f"Letters in incorrect positions:\n\t{incorrect_positions}\n")
+                # st.write (f"Letters to guess again:\n\t{sorted(list(next_letters), reverse = False)}\n")
+                st.write(f"Letters to not guess again:\n\t{sorted(list(dont_guess_again), reverse = False)}\n") # works
 
         # Returns True
-        # print(A.issubset(B)) # "if everything in A is in B", returns Bool
+        # st.write(A.issubset(B)) # "if everything in A is in B", returns Bool
 
         perfect_letts_per_guess.append(len(perfect_letters))
         wrong_pos_per_guess.append(len(incorrect_positions))
@@ -571,7 +570,7 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
                     if word not in guessed_words:
                         potential_next_guesses.add(word)
                                         
-            # print(f"GUESS {guess_num} : TEST 1-1")
+            # st.write(f"GUESS {guess_num} : TEST 1-1")
 
         if len(perfect_letters) == 0 and len(incorrect_positions) != 0: # if there are no perfect letters whatsoever, but there ARE incorrect positions ....
             for word in word_list:
@@ -593,7 +592,7 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
                                             else:
                                                 potential_next_guesses.add(word)
             
-            # print(f"GUESS {guess_num} : TEST 2-1")
+            # st.write(f"GUESS {guess_num} : TEST 2-1")
 
         else:
             for word in word_list:
@@ -603,7 +602,7 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
                         word_set.add(letter)
                         if next_letters.issubset(word_set):
                             if word not in guessed_words:
-                                # # print ("TEST 3-2")
+                                # # st.write ("TEST 3-2")
 
                                 if len(dont_guess_again) > 0:
                                     for bad_letter in dont_guess_again:
@@ -627,19 +626,19 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
                             if len(dummy_list) > 0:
                                 potential_next_guesses.remove(word)
                                         
-            # print(f"GUESS {guess_num} : TEST 3-1")
+            # st.write(f"GUESS {guess_num} : TEST 3-1")
 
         if return_stats == False:
             if verbose == True:
                 if len(potential_next_guesses) > 1:
-                    # print(f"At this point:")
-                    print(f"\t{len(word_list) - len(potential_next_guesses)}, {round((len(word_list) - len(potential_next_guesses)) / len(word_list) * 100, 2)}% of total words have been eliminated, and")
-                    print(f"\t{len(potential_next_guesses)}, {round(len(potential_next_guesses) / len(word_list) * 100, 2)}% of total words remain possible.\n")
+                    # st.write(f"At this point:")
+                    st.write(f"\t{len(word_list) - len(potential_next_guesses)}, {round((len(word_list) - len(potential_next_guesses)) / len(word_list) * 100, 2)}% of total words have been eliminated, and")
+                    st.write(f"\t{len(potential_next_guesses)}, {round(len(potential_next_guesses) / len(word_list) * 100, 2)}% of total words remain possible.\n")
                 
                 else:
-                    # print(f"At this point:")
-                    print(f"\t{len(word_list) - len(potential_next_guesses)}, {round((len(word_list) - len(potential_next_guesses)) / len(word_list) * 100, 2)}% of total words have been eliminated, and")
-                    print(f"\t{len(potential_next_guesses)}, {round(len(potential_next_guesses) / len(word_list) * 100, 2)}% of total words remain possible.\n")
+                    # st.write(f"At this point:")
+                    st.write(f"\t{len(word_list) - len(potential_next_guesses)}, {round((len(word_list) - len(potential_next_guesses)) / len(word_list) * 100, 2)}% of total words have been eliminated, and")
+                    st.write(f"\t{len(potential_next_guesses)}, {round(len(potential_next_guesses) / len(word_list) * 100, 2)}% of total words remain possible.\n")
         
         reduction_per_guess.append(len(potential_next_guesses))
                 
@@ -648,10 +647,10 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
 
             if return_stats == False:
                 if verbose == True:
-                    print(f"All potential next guesses:\n\t{get_word_rating(words_to_rate = list(potential_next_guesses), word_list = word_list)}\n")
-                    print(f"Words guessed so far:\n\t{guessed_words}.\n")
+                    st.write(f"All potential next guesses:\n\t{get_word_rating(words_to_rate = list(potential_next_guesses), word_list = word_list)}\n")
+                    st.write(f"Words guessed so far:\n\t{guessed_words}.\n")
                 
-                    print(f"The only remaining possible word is:\n\t'{list(potential_next_guesses)[0]}'\n")
+                    st.write(f"The only remaining possible word is:\n\t'{list(potential_next_guesses)[0]}'\n")
                 
             guess = list(potential_next_guesses)[0]
             # guess_entropies.append(get_word_rating([guess], word_list, normalized = False, ascending = False)[0][1])
@@ -659,7 +658,7 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
         else:
 
             best_next_guesses = list(potential_next_guesses)                
-            # # print (best_next_guesses)
+            # # st.write (best_next_guesses)
             word_ratings = get_word_rating(best_next_guesses, word_list, normalized = False, ascending = False) # "internal" ratings
             
             # Get max rating of all words
@@ -696,11 +695,11 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
             if return_stats == False:
                 if verbose == True:
                     if len(word_ratings) <= 40:
-                        print(f"All potential next guesses:\n\t{word_ratings}\n")
-                        print(f"Words guessed so far:\n\t{guessed_words}.\n")
+                        st.write(f"All potential next guesses:\n\t{word_ratings}\n")
+                        st.write(f"Words guessed so far:\n\t{guessed_words}.\n")
                     else:
-                        print(f"The top 40 potential next guesses are:\n\t{word_ratings[:40]}\n")
-                        print(f"Words guessed so far:\n\t{guessed_words}.\n")
+                        st.write(f"The top 40 potential next guesses are:\n\t{word_ratings[:40]}\n")
+                        st.write(f"Words guessed so far:\n\t{guessed_words}.\n")
 
             # guess_entropies.append(get_word_rating([guess], word_list, normalized = False, ascending = False)[0][1])
 
@@ -710,20 +709,20 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
             # stats_dict['target_guessed'] = False
             if return_stats == False:
                 if verbose == True:
-                    print("-----------------------------\n")
-                    print(f"Unfortunately, the Wordle could not be solved in {max_guesses} guesses.\n")
-                    print(f"The target word was '{target}'.\n")
-                    print("-----------------------------\n")
+                    st.write("-----------------------------\n")
+                    st.write(f"Unfortunately, the Wordle could not be solved in {max_guesses} guesses.\n")
+                    st.write(f"The target word was '{target}'.\n")
+                    st.write("-----------------------------\n")
                 else:
-                    print(f"\nUnfortunately, Wordle Wizard couldn't solve the puzzle in {max_guesses} guesses. Could you?")
-                    print(f"The target word was '{target}'.\n")
+                    st.write(f"\nUnfortunately, Wordle Wizard couldn't solve the puzzle in {max_guesses} guesses. Could you?")
+                    st.write(f"The target word was '{target}'.\n")
             break
         else: # if not at max guesses yet allowed
             # # stats_dict['target_guessed'] = False
             if return_stats == False:
                 if verbose == True:
-                    print(f"Next guess:\n\t'{guess}'")
-                    print("\n-----------------------------\n")
+                    st.write(f"Next guess:\n\t'{guess}'")
+                    st.write("\n-----------------------------\n")
 
         if guess == target:
             guess_num += 1
@@ -731,85 +730,20 @@ def wordle_wizard(word_list: list, max_guesses: int = None,
             # stats_dict['target_guessed'] = True
 
             if return_stats == False:
-                print(f"**Guess {guess_num}: '{guess}'**\n")
-                print(f"Wordle Wizard has solved the puzzle in {guess_num} guesses!")
+                st.write(f"**Guess {guess_num}: '{guess}'**\n")
+                st.write(f"Wordle Wizard has solved the puzzle in {guess_num} guesses!")
 
                 if max_guesses - guess_num == 1:
-                    print(f"There was only {max_guesses - guess_num} guess remaining.")
+                    st.write(f"There was only {max_guesses - guess_num} guess remaining.")
                 else:
-                    print(f"There were still {max_guesses - guess_num} guesses remaining.")
+                    st.write(f"There were still {max_guesses - guess_num} guesses remaining.")
 
             if return_stats == False:   
                 # # stats_dict['target_guessed'] = True                 
-                print(f"\nThe target word was **'{target}'**.")
-                print("\n-----------------------------")
+                st.write(f"\nThe target word was **'{target}'**.")
+                st.write("\n-----------------------------")
             break
-
-    # #### STATS STUFF    
-    # mid_guesses_vows = 0
-    # mid_guesses_cons = 0
-    # avg_perf_letters = 0
-    # avg_wrong_pos_letters = 0
-    # avg_wrong_letters = 0
-
-    # for i, word in enumerate(guessed_words):
-    #     mid_guesses_vows += count_vows_cons(word, y_vow = True)['vows']
-    #     mid_guesses_cons += count_vows_cons(word, y_vow = True)['cons']
         
-    # for i in range(0, len(guessed_words) - 1):
-    #     avg_perf_letters += perfect_letts_per_guess[i]
-    #     avg_wrong_pos_letters += wrong_pos_per_guess[i]
-    #     avg_wrong_letters += wrong_letts_per_guess[i]
-
-    # # stats_dict['mid_guesses_avg_vows'] = float(round(mid_guesses_vows / len(guessed_words), 2))
-    # # stats_dict['mid_guesses_avg_cons'] = float(round(mid_guesses_cons / len(guessed_words), 2))
-
-    # # stats_dict['avg_perf_letters'] = float(round(np.mean(avg_perf_letters), 2))
-    # # stats_dict['avg_wrong_pos_letters'] = float(round(np.mean(avg_wrong_pos_letters), 2))
-    # # stats_dict['avg_wrong_letters'] = float(round(np.mean(avg_wrong_letters), 2))
-    
-    # # average number of words remaining after each guess -- the higher this is, the luckier the person got (the lower, the more guesses it took)
-    # # stats_dict['avg_remaining'] = float(round(np.mean(reduction_per_guess), 2))
-
-    # # avg rating of each guessed word relative to all other words possible at that moment -- this should consistently be 100 for the algorithm, but will be different for user
-    # if len(guess_entropies) > 1: # in case of guessing it correctly on the first try
-    #     sum_entropies = 0
-    #     for rating in guess_entropies:
-    #         sum_entropies += rating
-
-    #     average_rating = float(round(sum_entropies / len(guess_entropies), 2))
-    #     # stats_dict['avg_intermediate_guess_rating'] = average_rating
-    # else:
-    #     # stats_dict['avg_intermediate_guess_rating'] = float(100)
-
-    # expected_guesses = 3.85
-
-    # # guess_num = 3
-    # # average_rating = 95
-    # luck = round(1 - ((((guess_num / expected_guesses) * (# stats_dict['avg_intermediate_guess_rating'] / 100)) / max_guesses) * 5), 2)
-    # # stats_dict['luck'] = luck
-
-    # if record == True:
-    #     if verbose == True:
-    #         with open(f"solutions/{guessed_words[0]}_{target}_wizard_detailed.txt", "w") as fout:
-            
-    #                 fout.write(line + "\n") # write each line of list of # printed text to .txt file
-    #     else:
-    #         with open(f"solutions/{guessed_words[0]}_{target}_wizard_summary.txt", "w") as fout:
-            
-    #                 fout.write(line + "\n") # write
-
-    # # if guess_num <= len(guess):
-    # if guess_num <= 6:
-    #     # stats_dict['valid_success'] = True
-    # else:
-    #     # stats_dict['valid_success'] = False
-
-    # # stats_dict['num_guesses'] = float(guess_num)
-
-    # # if return_stats == True:
-    #     return # stats_dict
-
 def wordle_wizard_cheat(guesses: list, word_list: list, max_guesses: int = None, 
                   target: str = None,
                   random_guess: bool = False, random_target: bool = False, 
